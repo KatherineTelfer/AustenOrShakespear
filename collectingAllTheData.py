c52c9cf0f99bca0a154c78047ee9c19fad537d1d
KatherineTelfer <168741538+KatherineTelfer@users.noreply.github.com>
2026-09-23 18:40:29 +0100

    set up the start of the data, included scikit learn and pandas as well as downloaded a few open source texts for the training data

---
+++ collectingAllTheData.py
@@ -0,0 +1,6 @@
+#collating data
+
+    # 1. import all of the data in a subfolder (e.g. for files in folder)
+    # 2. add them to new csv with information on who wrote ite
+    # 3. close the csv
+    # 4. return output so that you know it works
